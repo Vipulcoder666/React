c0f1760ce7ceb4d8fc312c79d469499c7d4f804e
Vipul Shrivastav <shrivastav.vipul252@gmail>
2024-09-27 00:21:03 +0530

    First day

---
+++ Basics/vipul project/src/App.jsx
@@ -0,0 +1,15 @@
+import Bca from './ujjawal.jsx'
+function App() {
+
+
+  return (
+    <>
+      <h1>chai aur react with vite</h1>
+      <h2>;iodfsna edkab</h2>
+      <Bca/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ Basics/vipul project/src/ujjawal.jsx
@@ -0,0 +1,6 @@
+function Bca() {
+    return(
+        <h1>Mishra</h1>
+    )
+}
+export default Bca
