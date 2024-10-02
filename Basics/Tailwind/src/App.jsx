e35a0e51141d0c04689eb7690bac8ea048aaaf9a
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-02 11:07:20 +0530

    1st Projects

--- Basics/Tailwind/src/App.jsx
+++ Basics/Tailwind/src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
 
   return (
-    <>
-      <h1>Hi Tailwind</h1>
-    </>
+    <div className="bg-slate-900 h-96 w-full">
+      <h1 className="text-red-700 text-4xl text-center sm:p-4 md:p-8 lg:p-16">Hi Tailwind</h1>
+      <h1 className="text-red-700 text-4xl text-center sm:p-4 md:p-8 lg:p-16">vipul</h1>
+    </div>
   )
 }
-
 export default App
---
+++ First/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import image from "./Assets/peter.jpg";
+import lap from "./Assets/lap.jpeg";
+function App() {
+
+  return (
+    <div className="bg-black text-white">
+     <h1 className="bg-black text-white">It's Me</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit nisi quas aperiam.</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident deleniti accusantium ut alias! Quam distinctio nostrum, sit ut labore id est debitis aliquid quis vero provident in aut eligendi quae dolore exercitationem sed cupiditate doloribus pariatur molestias dolorum ipsum architecto atque. Autem ipsam odio eum soluta a asperiores cum. Eum totam facilis nostrum sed quam non nihil dolor saepe magni numquam sequi officiis qui dicta recusandae atque quod hic eius, natus quidem, similique mollitia. Ab debitis maiores temporibus magnam reiciendis inventore, ex, aperiam ea quia est cum rem aliquam ipsam consequuntur nemo molestiae impedit eos rerum numquam sunt vero?</p>
+     <img src={image} alt="vipul" />
+     <img src={lap} alt="mishra" />
+    </div>
+  )
+}
+
+export default App
