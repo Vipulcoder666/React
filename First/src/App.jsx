100e29fb614817e537ad8680bb445dc366e94f8f
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-02 23:40:03 +0530

    update

--- First/src/App.jsx
+++ First/src/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import image from "./Assets/peter.jpg";
 import lap from "./Assets/lap.jpeg";
 import linkedIn from "./Assets/linkedIn.jpeg";
+import Vipul from "./Assets/Vipul.jpg";
 function App() {
 
   return (
-  <div class="bg-white dark:bg-black">
+<div class="bg-white dark:bg-black">
 
 
     <nav className="bg-gradient-to-l from-pink-500 to-blue-500 text-white h-12 flex items-center justify-between px-10">
-      {/* <div className="flex items-center justify-center space-x-6"> */}
-      <div>Logo</div>
+    <div class="font-bold text-3xl hidden md:inline">Vipul</div>
       <div className="space-x-6">
         <a href="#">Home</a>
         <a href="#">About us</a>
@@ -33,14 +33,23 @@ function App() {
         <button class="bg-gradient-to-r from-yellow-800 to-pink-500 rounded-lg h-8 w-24">
           <a href="https://drive.google.com/file/d/1RJ01nIO23TUA-RwmeSgWJvFqXFE5mSwG/view?usp=drive_link" target="_blank">Resume</a>
         </button>
-      </div>
-      
-
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, quos assumenda quasi ullam impedit accusantium nulla sit blanditiis odio officiis?</p>
+  <div>
+    </div>
     </div>
 
+    <div class="flex  items-center justify-center pt-16">
+      <h1 class="font-bold text-2xl">About me</h1>
+      <div>
+        <img src={Vipul} alt="Vipul"className="sm:h-40 w-40 md:h-50 w-50 " />
+      </div>
+      
+    </div>
+    </div>
 
-  </div>
+    
+
+
+</div>
   )
 }
 
