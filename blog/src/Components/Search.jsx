2af9580b7e869abb7495afa43da6ab4c28df6d4a
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-12 01:04:08 +0530

    Search done

--- blog/src/Components/Search.jsx
+++ blog/src/Components/Search.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Banner from './../assets/Banner.jpeg'
+import { VscSearch } from "react-icons/vsc";
 function Search() {
   return (
-    <div className='flex justify-center mt-10'>
-        <img src={Banner} alt="banner" className='w-[900px] rounded-2xl'/>
+    <div className='flex item-center justify-center mt-8 flex-col  px-[70] md:px-[150px]'>
+        <img src={Banner} alt="banner" className='rounded-2xl'/>
+        <div className='bg-white shadow-lg p-3 rounded-lg mt-[-20px] mx-[25%] flex items-center'>
+          <VscSearch className='text-[20px] text-gray-400'/>
+          <input type="text" placeholder='Search' className='outline-none ml-2'/>
+        </div>
     </div>
   )
 }
