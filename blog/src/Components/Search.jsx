a619539b015f4e19028a4ee1e47cbfc529693003
Vipul Shrivastav <vipulmpgi@gmail.com>
2024-10-11 23:24:05 +0530

    Banner Fixed

--- blog/src/Components/Search.jsx
+++ blog/src/Components/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Banner from './../assets/Banner.jpeg'
 function Search() {
   return (
-    <div>
-        
+    <div className='flex justify-center mt-10'>
+        <img src={Banner} alt="banner" className='w-[900px] rounded-2xl'/>
     </div>
   )
 }
