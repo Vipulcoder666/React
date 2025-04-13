22c2da5081da6451cb2251f05e50ac74727f1437
Vipul Shrivastav <vipulmpgi@gmail.com>
2025-04-13 15:21:12 +0530

    NavBar with Hamburer icons

--- Portfolio 2/port2/src/Components/Navbar/Navbar.jsx
+++ Portfolio 2/port2/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'remixicon/fonts/remixicon.css';
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+
   return (
-    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md-px-20'>
-        <span className='text-xl font-bold tracking-wide'>portfolio</span>
-        <ul className='mx-24 p-y2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl1 bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6'>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul>
+    <nav className="flex flex-wrap items-center justify-between text-white px-6 py-4 bg-[#171d32]">
+      {/* Logo */}
+      <span className="text-xl font-bold tracking-wide">Portfolio</span>
+
+      {/* Hamburger icon - visible on mobile */}
+      <div className="text-2xl cursor-pointer md:hidden" onClick={() => setMenu(!menu)}>
+        <i className="ri-menu-line"></i>
+      </div>
+
+      {/* Menu items */}
+      <ul
+        className={`${
+          menu ? 'block' : 'hidden'
+        } w-full md:flex md:items-center md:w-auto md:gap-6 mt-4 md:mt-0 text-center font-semibold`}
+      >
+        <li className="py-2 md:py-0">
+          <a href="#About" className="hover:text-red-400">About</a>
+        </li>
+        <li className="py-2 md:py-0">
+          <a href="#Experience" className="hover:text-red-400">Experience</a>
+        </li>
+        <li className="py-2 md:py-0">
+          <a href="#Projects" className="hover:text-red-400">Projects</a>
+        </li>
+        <li className="py-2 md:py-0">
+          <a href="#Contact" className="hover:text-red-400">Contact</a>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
